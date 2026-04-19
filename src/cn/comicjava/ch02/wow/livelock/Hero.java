2509cee793f768b2a886991e2cbc243680d42a6b
pkmer <q10viking@outlook.com>
2026-04-19 17:15:42 +0800

    docs: add livelock solution (courtesy limit) to ch02 notes

--- src/cn/comicjava/ch02/wow/livelock/Hero.java
+++ src/cn/comicjava/ch02/wow/livelock/Hero.java
@@ -1,8 +1,11 @@
 package cn.comicjava.ch02.wow.livelock;
 
+import java.util.Random;
+
 public class Hero {
     private String name;
     private boolean isThirsty;
+    private Random random = new Random();
 
     public Hero(String name) {
         this.name = name;
@@ -19,12 +22,24 @@ public class Hero {
 
 
     public void drink(Beer beer, Hero drinkingPartner) {
+        int courtesyCount = 0;  // 礼让次数计数
 		while (this.isThirsty()) {
 			if (beer.getOwner() != this) {
 				continue;
 			} else if (drinkingPartner.isThirsty()) {
-				System.out.printf("%s: \"你先喝吧，我的朋友！\"%n", name);
-				beer.setOwner(drinkingPartner);
+                if (courtesyCount < 3) {
+                    // 礼让不超过 3 次
+                    System.out.printf("%s: \"你先喝吧，我的朋友！\"%n", name);
+                    beer.setOwner(drinkingPartner);
+                    courtesyCount++;
+                } else {
+                    // 礼让 3 次后强制喝
+                    System.out.printf("%s: \"不客气了，我先喝！\"%n", name);
+                    beer.drink();
+                    this.isThirsty = false;
+                    System.out.printf("%s: \"真好喝！\"%n", name);
+                    beer.setOwner(drinkingPartner);
+                }
 			} else {
 				beer.drink();
 				this.isThirsty = false;
