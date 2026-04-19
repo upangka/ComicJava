8d915e540e1002d4919627a16665fff3a627284d
pkmer <q10viking@outlook.com>
2026-04-19 16:34:55 +0800

    docs: add ReentrantLock+tryLock solution for deadlock

--- src/cn/comicjava/ch02/wow/lockjuc/Hero.java
+++ src/cn/comicjava/ch02/wow/lockjuc/Hero.java
@@ -1,7 +1,12 @@
 package cn.comicjava.ch02.wow.lockjuc;
 
+import java.util.concurrent.locks.Lock;
+
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Hero {
     private String name;
+    private Lock lock = new ReentrantLock();
 
     public Hero(String name) {
         this.name = name;
@@ -11,17 +16,57 @@ public class Hero {
         return name;
     }
 
-    public synchronized void visit(Hero hero){
+    private boolean meetAt(Hero hero) {
+        boolean iSayWeMeetAtHisPlace = false;
+        boolean heSaysWeMeetAtHisPlace = false;
         try {
-            Thread.sleep(50);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            iSayWeMeetAtHisPlace = lock.tryLock();
+            heSaysWeMeetAtHisPlace = hero.getLock().tryLock();
+        } finally {
+            if (!(iSayWeMeetAtHisPlace && heSaysWeMeetAtHisPlace)) {
+                if (iSayWeMeetAtHisPlace) {
+                    lock.unlock();
+                }
+                if (heSaysWeMeetAtHisPlace) {
+                    hero.getLock().unlock();
+                }
+            }
         }
-        System.out.printf("%s: \"我们在%s那儿见面，那我就先出发了。\"%n", this.getName(), hero.getName());
-        hero.receiveVisit(this);
+        // ⚠️ 注意：返回 true 时锁仍在持有状态，需要调用者释放
+        return iSayWeMeetAtHisPlace && heSaysWeMeetAtHisPlace;
     }
 
-    public synchronized void receiveVisit(Hero hero){
-        System.out.printf("%s: \"我们在我家见面了。\"%n", this.getName());
+    public Lock getLock() {
+        return lock;
+    }
+
+    public void unlockBoth(Hero hero) {
+        try {
+            hero.getLock().unlock(); // 先释放 hero 的锁
+        } finally {
+            lock.unlock(); // 最后释放自己的锁
+        }
+    }
+
+    public void visit(Hero hero) {
+        if (meetAt(hero)) {
+            try {
+                System.out.printf("(%s)%s: \"我们在%s那儿见面，那我就先出发了。\"%n", getThreadName(), this.getName(), hero.getName());
+                hero.receiveVisit(this);
+            } finally {
+                unlockBoth(hero);
+            }
+        } else {
+            System.out.printf("(%s)%s: \"抱歉，我在忙，下次再约吧。\"%n", getThreadName(), this.getName());
+        }
+
+    }
+
+    public void receiveVisit(Hero hero) {
+        System.out.printf("(%s)%s: \"我们在我家见面了。\"%n", getThreadName(), this.getName());
+    }
+
+    private String getThreadName() {
+        return Thread.currentThread().getName();
     }
 }
--- src/cn/comicjava/ch02/wow/lockjuc/Main.java
+++ src/cn/comicjava/ch02/wow/lockjuc/Main.java
@@ -5,7 +5,7 @@ public class Main {
         final Dwarf dwarf = new Dwarf("矮人");
         final NightElf nightElf = new NightElf("暗夜精灵");
         
-       new Thread(() -> dwarf.visit(nightElf), "矮人线程").start();
-       new Thread(() -> nightElf.visit(dwarf), "暗夜精灵线程").start();
+        new Thread(() -> dwarf.visit(nightElf), "任务1").start();
+        new Thread(() -> nightElf.visit(dwarf), "任务2").start();
     }
 }
