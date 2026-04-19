13e0b5165bf011230459fe72209138e77786a198
pkmer <q10viking@outlook.com>
2026-04-19 16:45:48 +0800

    chore: fix package names in livelock package

---
+++ src/cn/comicjava/ch02/wow/livelock/Dwarf.java
@@ -0,0 +1,12 @@
+package cn.comicjava.ch02.wow.livelock;
+
+/**
+ * 矮人
+ */
+public class Dwarf extends Hero {
+
+	public Dwarf(String name) {
+		super(name);
+	}
+
+}
---
+++ src/cn/comicjava/ch02/wow/livelock/NightElf.java
@@ -0,0 +1,7 @@
+package cn.comicjava.ch02.wow.livelock;
+
+public class NightElf extends Hero {
+    public NightElf(String name) {
+        super(name);
+    }
+}
