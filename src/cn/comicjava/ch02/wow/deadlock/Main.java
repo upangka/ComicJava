6c3be3f533cbcc99a641a5ebbecda6a58b4afbb9
pkmer <q10viking@outlook.com>
2026-04-19 15:28:04 +0800

    chore: add deadlock diagram and update notes

--- src/cn/comicjava/ch02/wow/deadlock/Main.java
+++ src/cn/comicjava/ch02/wow/deadlock/Main.java
@@ -5,7 +5,7 @@ public class Main {
         final Dwarf dwarf = new Dwarf("矮人");
         final NightElf nightElf = new NightElf("暗夜精灵");
         
-       new Thread(() -> dwarf.visit(nightElf)).start();
-       new Thread(() -> nightElf.visit(dwarf)).start();
+       new Thread(() -> dwarf.visit(nightElf), "矮人线程").start();
+       new Thread(() -> nightElf.visit(dwarf), "暗夜精灵线程").start();
     }
 }
