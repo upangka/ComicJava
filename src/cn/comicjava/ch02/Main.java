b0d9efc78ae7a27a1d2e30221bd612323d32c6bb
pkmer <q10viking@outlook.com>
2026-04-19 13:08:25 +0800

    chore: update Main.java with InterruptedException and cleanup images

--- src/cn/comicjava/ch02/Main.java
+++ src/cn/comicjava/ch02/Main.java
@@ -1,7 +1,7 @@
 package cn.comicjava.ch02;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         new Thread(new DoSomething()).start();
         new DoSomethingToo().start();
     }
