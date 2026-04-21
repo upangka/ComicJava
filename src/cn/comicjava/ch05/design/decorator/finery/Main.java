62916076aa5d7e38fb28f7bb76e96ef019910fe3
pkmer <q10viking@outlook.com>
2026-04-21 12:15:53 +0800

    docs: 添加装饰器模式笔记及规则更新

--- src/cn/comicjava/ch05/design/decorator/finery/Main.java
+++ src/cn/comicjava/ch05/design/decorator/finery/Main.java
@@ -6,9 +6,7 @@ public class Main {
         Person pkmer = new Person("Pkmer");
 
         TShirts tShirts = new TShirts();
-        
         BigTrouser bigTrouser = new BigTrouser();
-        
         Sneakers sneakers = new Sneakers();
         
         System.out.println("---------第一种装扮---------------");
@@ -16,5 +14,16 @@ public class Main {
         bigTrouser.decorator(tShirts);
         sneakers.decorator(bigTrouser);
         sneakers.show();
+
+
+        System.out.println("---------第二种装扮---------------");
+        LeatherShoes leatherShoes = new LeatherShoes();
+        Tie tie = new Tie();
+        Suit suit = new Suit();
+
+        leatherShoes.decorator(pkmer);
+        tie.decorator(leatherShoes);
+        suit.decorator(tie);
+        suit.show();
     }
 }
